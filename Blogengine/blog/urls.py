61bcfaaa0efc6e02161fdd317f120fe8b30ecebf
egorgargolin <98bariors@gmail.com>
2022-07-13 11:20:04 +0200

    Now we can see current post page

--- Blogengine/blog/urls.py
+++ Blogengine/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', posts_list)
+    path('', posts_list, name = 'posts_list_url'),
+    path('post/<str:slug>/', current_post, name = 'current_post_url')
 ]
--- Blogengine/blog/views.py
+++ Blogengine/blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 # Create your views here.
 def posts_list(request):
-    n = ['Oleg', 'Egor', 'Varya']
-    return render(request, 'blog/index.html', context={'names' : n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts' : posts})
+
+def current_post(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/current_post.html', context={'post' : post})
