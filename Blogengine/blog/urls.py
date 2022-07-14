181be6ddb3684178a6a451a2f8a6803b66aaf48a
egorgargolin <98bariors@gmail.com>
2022-07-14 10:04:28 +0200

    Fixed views and added Mixins

--- Blogengine/blog/urls.py
+++ Blogengine/blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', posts_list, name = 'posts_list_url'),
-    path('post/<str:slug>/', current_post, name = 'current_post_url'),
+    path('', posts_list, name='posts_list_url'),
+    path('post/<str:slug>/', PostDetail.as_view(), name='current_post_url'),
     path('tags/', tag_list, name='tag_list_url'),
-    path('tag/<str:slug>', tag_detail, 'tag_detail_url')
+    path('tag/<str:slug>', TagDetail.as_view(), name='tag_detail_url')
 ]
--- Blogengine/blog/views.py
+++ Blogengine/blog/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import View
+from django.shortcuts import get_object_or_404
+
 from .models import Post, Tag
-
-
+from .utils import ObjectDetailMixin
 # Create your views here.
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts' : posts})
 
-def current_post(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/current_post.html', context={'post' : post})
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/current_post.html'
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
 
 def tag_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tag_list.html', context={'tags' : tags})
 
-#def tag_detail(request, )
